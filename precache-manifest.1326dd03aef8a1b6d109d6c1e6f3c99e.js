485211f6d9dfc77adefb8bea496e892dab68c2fc
gutza <digutza.2@gmail.com>
2020-07-31 01:02:55 -0500

    Updates

---
+++ precache-manifest.1326dd03aef8a1b6d109d6c1e6f3c99e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f48f680de8138e98da84aa87d9466a",
+    "url": "/MyStoreUV-app/index.html"
+  },
+  {
+    "revision": "811c65eb07b4cada5ee1",
+    "url": "/MyStoreUV-app/static/css/main.cc869ada.chunk.css"
+  },
+  {
+    "revision": "cbc6c334b436f0a1a23c",
+    "url": "/MyStoreUV-app/static/js/2.535fab5e.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/MyStoreUV-app/static/js/2.535fab5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811c65eb07b4cada5ee1",
+    "url": "/MyStoreUV-app/static/js/main.59010474.chunk.js"
+  },
+  {
+    "revision": "ff083a7115cecb7833aa",
+    "url": "/MyStoreUV-app/static/js/runtime-main.7c4f7303.js"
+  },
+  {
+    "revision": "960f20d9cdc2bb6d804485f66ffa8e5d",
+    "url": "/MyStoreUV-app/static/media/avatarDG.960f20d9.png"
+  },
+  {
+    "revision": "d37e446be9920fb3cfd0d8d0da651746",
+    "url": "/MyStoreUV-app/static/media/avatarGabriel.d37e446b.png"
+  },
+  {
+    "revision": "6dbc38ef9d8f2cc302b3c45d9ac245bf",
+    "url": "/MyStoreUV-app/static/media/avatarMarta.6dbc38ef.png"
+  },
+  {
+    "revision": "6ada6a973ec8515a280736d2321ee669",
+    "url": "/MyStoreUV-app/static/media/banner.6ada6a97.jpg"
+  }
+]);
